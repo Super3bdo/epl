f712337724625b98c001fe16d965a047b48b0985
abdul.mahmoud14890@gmail.com <abdul.mahmoud14890@gmail.com>
2017-07-25 21:23:29 +0200

    Rename Package

---
+++ src/main/java/config/JerseyConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        this.register(HelloResource.class);
+    }
+}
